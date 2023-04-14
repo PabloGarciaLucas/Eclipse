a2047cf46884652a57f9d73c2797715cbef743fc
PabloGarciaLucas <pablo.garluc@educa.jcyl.es>
2023-04-14 20:26:34 +0200

    Validando el mensaje

---
+++ src/main/java/Control/ControlVersiones.java
@@ -0,0 +1,12 @@
+package Control;
+
+public class ControlVersiones {
+	public static void main(String[] args) {
+		System.out.println("hola primero");
+		System.out.println("hola segundo cambio");
+		System.out.println("hola segundo cambia1");
+		System.out.println("hola segundo cambia2");
+		System.out.println("hola segundo cambia3");
+		System.out.println("hola segundo cambia3");
+	}
+}
